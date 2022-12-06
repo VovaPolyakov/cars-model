c2d28d6c69df7f6df31d76ef2c58ff63c5f6c450
Vladimir <1337vovalenovo@gmail.com>
2022-12-06 21:34:01 +0300

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import './App.css';
-import Table from './components/Table/Table'
-import ShowWindow from './components/ShowWindow/ShowWindow';
-import { useState } from 'react';
+import Table from './components/Table/Table';
 
 function App() {
-  const [data,setData] = useState('')
-
-  const handleChange = (e) => {
-    // console.log(e.target.name)
-    setData(e.target.name)
-  }
   return (
     <div className="App">
       <div className='border-8 border-black w-3/5 m-auto flex  p-5'>
-        <div className='w-3/5'>
-          <Table item={data} />
-        </div>
-        <ShowWindow onChange={handleChange}/>
+        <Table/>
       </div>
     </div>
   );
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,28 +1,45 @@
 import React from 'react'
 import cars from '../../cars'
 import { useState } from 'react'
+import ShowWindow from '../ShowWindow/ShowWindow'
 
-const Table = ({item}) => {
-    console.log(item)
+
+const Table = () => {
+    const [show,setShow] = useState({
+        id: true,
+        make:true,
+        model:true,
+        year:true,
+        price:true,
+    })
+    const handleClick = (e) => {
+        setShow({
+            ...show,
+            [e.target.name]: !show[e.target.name]
+        })
+    }
     return (
-    <table className='border-4 border-black m-auto w-full'>
-        <tr className=' border-2'>
-            <th className=' border-2'>ID</th>
-            <th className=' border-2'>Make</th>
-            <th className=' border-2'>Model</th>
-            <th className=' border-2'>Year</th>
-            <th className=' border-2'>Price</th>
-        </tr>
-        {cars.map((car) => (
-            <tr className=' border-2'>
-                <td className=' border-2 p-3'>{car.id}</td>
-                <td  className=' border-2'></td>
-                <td  className=' border-2'>{car.name}</td>
-                <td  className=' border-2'>{car.year}</td>
-                <td  className=' border-2'></td>
-            </tr>
-            ))}
-    </table>
+        <div className='flex w-full'>
+            <ShowWindow onChange={handleClick}/>
+             <table className='border-4 border-black m-auto w-3/5'>
+                <tr className=' border-2'>
+                    {show.id && <th className=' border-2'>ID</th>}
+                    {show.make && <th className=' border-2'>Make</th>}
+                    {show.model && <th className=' border-2'>Model</th>}
+                    {show.year && <th className=' border-2'>Year</th>}
+                    {show.price && <th className=' border-2'>Price</th>}
+                </tr>
+                {cars.map((car) => (
+                <tr className=' border-2'>
+                    <td className=' border-2 p-3'>{car.id}</td>
+                    <td  className=' border-2'></td>
+                    <td  className=' border-2'>{car.name}</td>
+                    <td  className=' border-2'>{car.year}</td>
+                    <td  className=' border-2'></td>
+                </tr>
+                ))}
+            </table>
+        </div>
   )
 }
 
