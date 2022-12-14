eb35971b61c97d860a5882e9fa26278dc6108f26
Vladimir <1337vovalenovo@gmail.com>
2022-12-14 23:28:28 +0300

    added id reverse

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -12,31 +12,56 @@ const Table = () => {
         year:true,
         price:true,
     })
+    const [sort,setSort] = useState('asc')
+    const [reverseCars, setReverseCars] = useState(cars)
     const handleClick = (e) => {
         setShow({
             ...show,
             [e.target.name]: !show[e.target.name]
         })
     }
+
+    // const sortArray = () => {
+    //     setSort(sort === 'asc' ? 'desc' : 'asc')
+    // }
+    const sortId = () => {
+        setSort(sort === 'asc' ? 'desc' : 'asc')
+        const carsID = []
+        cars.forEach(car => {
+            carsID.push(car.id)
+        });
+        // const renderData = () => carsID.reverse();
+        // console.log(renderData())
+        carsID.reverse();
+        for(let i = 0;i < carsID.length;i++){
+            cars.map(car => {
+                cars[i].id = carsID[i]
+        })
+        setReverseCars(cars)
+    }
+
+    }
     return (
         <div className='flex w-full'>
             <ShowWindow onChange={handleClick}/>
              <table className='border-4 border-black m-auto w-3/5'>
                 <tr className=' border-2'>
-                    {show.id && <th className=' border-2'>ID</th>}
+                    {show.id && <th className=' border-2'>ID
+                        <button onClick={sortId} className='pl-5'>Sort items {sort}</button>
+                    </th>}
                     {show.make && <th className=' border-2'>Make</th>}
                     {show.model && <th className=' border-2'>Model</th>}
                     {show.year && <th className=' border-2'>Year</th>}
                     {show.price && <th className=' border-2'>Price</th>}
                 </tr>
-                {cars.map((car) => (
-                <tr className=' border-2'>
-                    <td className=' border-2 p-3'>{car.id}</td>
-                    <td  className=' border-2'></td>
-                    <td  className=' border-2'>{car.name}</td>
-                    <td  className=' border-2'>{car.year}</td>
-                    <td  className=' border-2'></td>
-                </tr>
+                {reverseCars.map((car) => (
+                    <tr className=' border-2'>
+                        <td className=' border-2 p-3'>{car.id}</td>
+                        <td  className=' border-2'></td>
+                        <td  className=' border-2'>{car.name}</td>
+                        <td  className=' border-2'>{car.year}</td>
+                        <td  className=' border-2'></td>
+                    </tr>
                 ))}
             </table>
         </div>
